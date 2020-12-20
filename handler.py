1710e0c4c1725f24b7d57706a2ef8ecacc81e7ec
adri <aizpurua23a@gmail.com>
2020-12-20 17:23:17 +0100

    updated 20201220

--- handler.py
+++ handler.py
@@ -21,6 +21,31 @@ ERROR_RESOPNSE = {
     "body": json.dumps('Oops, something went wrong!')
 }
 
+character_deaths = os.environ.get("CHARACTER_DEATHS")
+sessions = os.environ.get('SESSIONS')
+
+START_ANSWER = ["Hello, human! I'm a sassy bot meant to shade the fuck out of a DM who loves to murder characters :)"]
+DEATH_COUNT_ANSWER = [
+    "That's a very good question!",
+    "So far, there have been {} character deaths.".format(character_deaths),
+    "That's a heckin' lot!",
+    "Specially taking into account that we're {} sessions in.".format(sessions),
+    "That's a CDPS of {:0.2f}%. Deadly!".format(100 * int(character_deaths)/int(sessions))
+]
+
+EULOGY_ANSWER = [
+    "Let's remember those who fought bravely and fell so that we can fight on:",
+    "- Baccus Garibaldi, Banca Privada. Your moneyed ways will be forever remembered.",
+    "- Kala Banca Privada. Through Hammer and Sickle, your short but sweet fight continues.",
+    "- Talion. He fought bravely for Torm. Undril griefs your passing.",
+    "- Albondiel Eru-Ilúvatar. Wintery asshole. Died shortly before her love.",
+    "- Droplette. Died in battle fightng fiercely. Your death shall have its vengeance.",
+    "- Bismarck. He was the best boy. He smiles at us from his afterlife in doggy heaven.",
+    "- F for Summerwise. Our time together was short, but well spent. "
+    "May you rest and meet Moonbeam Lillyfall in the endless prairies of Dragon-Heaven.\n",
+    "May Othgoroth have mercy on their souls."
+]
+
 
 def configure_telegram():
     telegram_token = os.environ.get('TELEGRAM_TOKEN')
@@ -49,30 +74,13 @@ def hello(event, context):
         answer = []
 
         if '/start' in text:
-            answer = ["""Hello, human! I'm a sassy bot meant to shade the fuck out of a DM
-            who loves to murder characters :)"""]
+            answer = START_ANSWER
 
         if "I wonder how we're doing in Chult." in text:
-            character_deaths = os.environ.get("CHARACTER_DEATHS")
-            sessions = os.environ.get('SESSIONS')
-            answer = [
-                "That's a very good question!",
-                "So far, there have been {} character deaths.".format(character_deaths),
-                "That's a heckin' lot!",
-                "Specially taking into account that we're {} sessions in.".format(sessions),
-                "That's a CDPS of {:0.2f}%. Deadly!".format(100 * int(character_deaths)/int(sessions))
-            ]
+            answer = DEATH_COUNT_ANSWER
 
         if "Let's remember the departed." in text:
-            answer = [
-                "Let's remember those who fought bravely and fell so that we can fight on:",
-                "- Baccus Garibaldi, Banca Privada. Your moneyed ways will be forever remembered.",
-                "- Kala Banca Privada. Through Hammer and Sickle, your short but sweet fight continues.",
-                "- Talion. He fought bravely for Torm. Undril griefs your passing.",
-                "- Albondiel Eru-Ilúvatar. Wintery asshole. Died shortly before her love.",
-                "- Droplette. Died in battle fightng fiercely. Your death shall have its vengeance.",
-                "May Othgoroth have mercy on their souls."
-            ]
+            answer = EULOGY_ANSWER
 
         if answer:
             bot.sendMessage(chat_id=chat_id, text='\n'.join(answer))
